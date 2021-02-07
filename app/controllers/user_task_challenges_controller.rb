bc0cf21ad9b73b326458de4574160e55da91fb1e
Ekenayy <ekenayy@gmail.com>
2021-02-07 17:13:33 -0500

    created show and fake action for users as well as create UTC

--- app/controllers/user_task_challenges_controller.rb
+++ app/controllers/user_task_challenges_controller.rb
@@ -1,2 +1,20 @@
 class UserTaskChallengesController < ApplicationController
+
+    def create
+        user_task_challenge = UserTaskChallenge.create(utc_params)
+
+        if user_task_challenge
+            render json: user_task_challenge
+        else 
+            render json: {errors: user_task_challenge.errors.full_messages }
+        end 
+        
+    end
+
+    private
+
+    def utc_params
+        params.permit(:description, :user_challenge_id, :completed)
+    end 
+    
 end
