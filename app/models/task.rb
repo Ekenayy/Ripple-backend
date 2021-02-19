d9461c19d4128e1433a4f3e9871b94423df1f207
Ekenayy <ekenayy@gmail.com>
2021-02-18 20:08:30 -0500

    Created description method for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
     has_many :task_challenges, dependent: :destroy
     
+    def self.all_Descriptions
+        self.all.map { |task| task.description}
+    end 
+    
 end
